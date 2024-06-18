451a7922d493ef5a6e9359c73b2659e605d53a2b
plusdojo2024-takagi <takagi-yuuki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-18 17:30:15 +0900

    髙木_Orderlist/Memos

---
+++ src/model/Orderlist.java
@@ -0,0 +1,30 @@
+package model;
+
+import java.io.Serializable;
+
+public class Orderlist implements Serializable{
+
+//	private int number; /* ID */
+//	private String company; /* CREATED_AT */
+//	private String department; /* UPDATED_AT */
+//	private String position;  /* 内装テーブル */
+//	private String name; /* 内装テーブルクロス */
+//	private String zipcode;  /* 内装チェア */
+//	private String address;  /* 内装ナプキン */
+//	private String phone; /* 内装フラワー */
+//	private String fax; // /* 衣装 */
+//	private String email;  /* ケーキ全体 */
+//	private String schedule; /* ケーキ上部 */
+//	private String hobby;  /* 招待状 */
+//	private String remarks;  /* ネームプレート */
+//	private String email;  /* 人数配置 */
+//	private String schedule; /* 発注期限日 */
+//	private String hobby;  /* メモID */
+//	private String remarks;  /* 人数 */
+
+	// 引数がないコンストラクタ 仕様だから作る 使うことはない
+//    public Bc() {
+
+    }
+
+//}
