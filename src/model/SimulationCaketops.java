5dc577183539dfa2670529191df803c012e0f2ef
plusdojo2024-koyama <koyama-shoma-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-20 17:23:03 +0900

    小山　ケーキ上部作成途中

--- src/model/SimulationCaketops.java
+++ src/model/SimulationCaketops.java
@@ -6,18 +6,25 @@ public class SimulationCaketops implements Serializable {
     private int id;				// シミュレーション総合結果ID
     private String created_at;	// データ作成日時
     private String updated_at;	// データ更新日時
-    private int memo_id;		// ケーキ上部の詳細メモのID
+    private String memo_id;		// ケーキ上部の詳細メモのID
+    private String memo;		// ケーキ上部の詳細メモ
 
     public SimulationCaketops() {
 
     }
 
-	public SimulationCaketops(int id, String created_at, String updated_at, int memo_id) {
+	public SimulationCaketops(int id, String created_at, String updated_at, String memo_id) {
 		super();
 		this.id = id;
 		this.created_at = created_at;
 		this.updated_at = updated_at;
 		this.memo_id = memo_id;
+	}
+
+	public SimulationCaketops(int id, String memo) {
+		super();
+		this.id = id;
+		this.memo = memo;
 	}
 
 	public int getId() {
@@ -44,11 +51,19 @@ public class SimulationCaketops implements Serializable {
 		this.updated_at = updated_at;
 	}
 
-	public int getMemo_id() {
+	public String getMemo_id() {
 		return memo_id;
 	}
 
-	public void setMemo_id(int memo_id) {
+	public void setMemo_id(String memo_id) {
 		this.memo_id = memo_id;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
 	}
 }
