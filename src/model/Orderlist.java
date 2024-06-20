1ce64755cf2a606ef6f07117056a1d36d246d0c2
plusdojo2024-takagi <takagi-yuuki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-20 16:41:46 +0900

    髙木_0620

--- src/model/Orderlist.java
+++ src/model/Orderlist.java
@@ -1,38 +1,38 @@
 package model;
 
 import java.io.Serializable;
+import java.sql.Date;
+import java.sql.Timestamp;
 
 public class Orderlist implements Serializable {
 
-	private int id ; /* ID */
-	private String created_at; /* CREATED_AT */
-	private String idupdated_at; /* UPDATED_AT */
-	private String customer_id; /* お客様ID */
-	private String table_id; /* 内装テーブル */
-	private String tablecover_id; /* 内装テーブルクロス */
-	private String chair_id; /* 内装チェア */
-	private String napkin_id; /* 内装ナプキン */
-	private String flower_id; // /* 内装フラワー */
-	private String clothes_id; /* 衣装 */
-	private String cake_id; /* ケーキ全体 */
-	private String caketop_id; /* ケーキ上部 */
-	private String invitation_id; /* 招待状 */
-	private String nameplate_id; /* ネームプレート */
-	private String tablemember_id; /* 人数配置 */
-	private String deadlines; /* 発注期限日 */
-	private String memo_id; /* メモID */
-	private String number; /* 人数 */
+	private int id; /* ID */
+	private Timestamp created_at; /* CREATED_AT */
+	private Timestamp idupdated_at; /* UPDATED_AT */
+	private int customer_id; /* お客様ID */
+	private int table_id; /* 内装テーブル */
+	private int tablecover_id; /* 内装テーブルクロス */
+	private int chair_id; /* 内装チェア */
+	private int napkin_id; /* 内装ナプキン */
+	private int flower_id; // /* 内装フラワー */
+	private int clothes_id; /* 衣装 */
+	private int cake_id; /* ケーキ全体 */
+	private int caketop_id; /* ケーキ上部 */
+	private int invitation_id; /* 招待状 */
+	private int nameplate_id; /* ネームプレート */
+	private int tablemember_id; /* 人数配置 */
+	private Date deadlines; /* 発注期限日 */
+	private int memo_id; /* メモID */
+	private int number; /* 人数 */
 
 	// 引数がないコンストラクタ 仕様だから作る 使うことはない
-    public Orderlist() {
+	public Orderlist() {
 
-    }
+	}
 
-	public Orderlist(int id, String created_at, String idupdated_at, String customer_id, String table_id, String tablecover_id,
-			String chair_id, String napkin_id, String flower_id, String clothes_id, String cake_id, String caketop_id,
-			String invitation_id, String nameplate_id, String tablemember_id, String deadlines, String memo_id,
-			String number) {
-
+	public Orderlist(int id, Timestamp created_at, Timestamp idupdated_at, int customer_id, int table_id,
+			int tablecover_id, int chair_id, int napkin_id, int flower_id, int clothes_id, int cake_id, int caketop_id,
+			int invitation_id, int nameplate_id, int tablemember_id, Date deadlines, int memo_id, int number) {
 		super();
 		this.id = id;
 		this.created_at = created_at;
@@ -54,15 +54,6 @@ public class Orderlist implements Serializable {
 		this.number = number;
 	}
 
-
-
-	public Orderlist(int int1, String string, String string2, String string3, String string4, String string5,
-			String string6, String string7, String string8, String string9, String string10, String string11,
-			String string12, String string13, String string14, String string15, String string16) {
-	}
-
-
-
 	public int getId() {
 		return id;
 	}
@@ -71,140 +62,139 @@ public class Orderlist implements Serializable {
 		this.id = id;
 	}
 
-	public String getCreated_at() {
+	public Timestamp getCreated_at() {
 		return created_at;
 	}
 
-	public void setCreated_at(String created_at) {
+	public void setCreated_at(Timestamp created_at) {
 		this.created_at = created_at;
 	}
 
-	public String getIdupdated_at() {
+	public Timestamp getIdupdated_at() {
 		return idupdated_at;
 	}
 
-	public void setIdupdated_at(String idupdated_at) {
+	public void setIdupdated_at(Timestamp idupdated_at) {
 		this.idupdated_at = idupdated_at;
 	}
 
-	public String getCustomer_id() {
+	public int getCustomer_id() {
 		return customer_id;
 	}
 
-	public void setCustomer_id(String customer_id) {
+	public void setCustomer_id(int customer_id) {
 		this.customer_id = customer_id;
 	}
 
-	public String getTable_id() {
+	public int getTable_id() {
 		return table_id;
 	}
 
-	public void setTable_id(String table_id) {
+	public void setTable_id(int table_id) {
 		this.table_id = table_id;
 	}
 
-	public String getTablecover_id() {
+	public int getTablecover_id() {
 		return tablecover_id;
 	}
 
-	public void setTablecover_id(String tablecover_id) {
+	public void setTablecover_id(int tablecover_id) {
 		this.tablecover_id = tablecover_id;
 	}
 
-	public String getChair_id() {
+	public int getChair_id() {
 		return chair_id;
 	}
 
-	public void setChair_id(String chair_id) {
+	public void setChair_id(int chair_id) {
 		this.chair_id = chair_id;
 	}
 
-	public String getNapkin_id() {
+	public int getNapkin_id() {
 		return napkin_id;
 	}
 
-	public void setNapkin_id(String napkin_id) {
+	public void setNapkin_id(int napkin_id) {
 		this.napkin_id = napkin_id;
 	}
 
-	public String getFlower_id() {
+	public int getFlower_id() {
 		return flower_id;
 	}
 
-	public void setFlower_id(String flower_id) {
+	public void setFlower_id(int flower_id) {
 		this.flower_id = flower_id;
 	}
 
-	public String getClothes_id() {
+	public int getClothes_id() {
 		return clothes_id;
 	}
 
-	public void setClothes_id(String clothes_id) {
+	public void setClothes_id(int clothes_id) {
 		this.clothes_id = clothes_id;
 	}
 
-	public String getCake_id() {
+	public int getCake_id() {
 		return cake_id;
 	}
 
-	public void setCake_id(String cake_id) {
+	public void setCake_id(int cake_id) {
 		this.cake_id = cake_id;
 	}
 
-	public String getCaketop_id() {
+	public int getCaketop_id() {
 		return caketop_id;
 	}
 
-	public void setCaketop_id(String caketop_id) {
+	public void setCaketop_id(int caketop_id) {
 		this.caketop_id = caketop_id;
 	}
 
-	public String getInvitation_id() {
+	public int getInvitation_id() {
 		return invitation_id;
 	}
 
-	public void setInvitation_id(String invitation_id) {
+	public void setInvitation_id(int invitation_id) {
 		this.invitation_id = invitation_id;
 	}
 
-	public String getNameplate_id() {
+	public int getNameplate_id() {
 		return nameplate_id;
 	}
 
-	public void setNameplate_id(String nameplate_id) {
+	public void setNameplate_id(int nameplate_id) {
 		this.nameplate_id = nameplate_id;
 	}
 
-	public String getTablemember_id() {
+	public int getTablemember_id() {
 		return tablemember_id;
 	}
 
-	public void setTablemember_id(String tablemember_id) {
+	public void setTablemember_id(int tablemember_id) {
 		this.tablemember_id = tablemember_id;
 	}
 
-	public String getDeadlines() {
+	public Date getDeadlines() {
 		return deadlines;
 	}
 
-	public void setDeadlines(String deadlines) {
+	public void setDeadlines(Date deadlines) {
 		this.deadlines = deadlines;
 	}
 
-	public String getMemo_id() {
+	public int getMemo_id() {
 		return memo_id;
 	}
 
-	public void setMemo_id(String memo_id) {
+	public void setMemo_id(int memo_id) {
 		this.memo_id = memo_id;
 	}
 
-	public String getNumber() {
+	public int getNumber() {
 		return number;
 	}
 
-	public void setNumber(String number) {
+	public void setNumber(int number) {
 		this.number = number;
 	}
 }
-
