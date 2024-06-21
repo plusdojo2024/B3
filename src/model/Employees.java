11fc232099bb481c89320e383a071a35d8467694
plusdojo2024-ganbat <ganbat-nomin-eredene-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-21 16:43:56 +0900

    ノミン＿シミュレーション

--- src/model/Employees.java
+++ src/model/Employees.java
@@ -1,33 +1,56 @@
 package model;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
 
 public class Employees implements Serializable {
-    private int company_id;    // ID
+	private int id;
+    private int companyId;
     private String user;
-    private String pw;    // PW
+    private String pw;
+    private Timestamp createdAt;
+    private Timestamp updateAt;
 
-    public Employees(int company_id, String user, String pw) {
-        this.company_id = company_id;
+    public Employees(int id, int companyId, String user, String pw, Timestamp createdAt, Timestamp updatedAt) {
+        this.id = id;
+        this.companyId = companyId;
         this.user = user;
         this.pw = pw;
+        this.createdAt = createdAt;
+        this.updateAt = updatedAt;
     }
-    public Employees(String user, String pw) {
+    public Employees(int companyId,String user, String pw) {
+    	this.companyId = companyId;
         this.user = user;
         this.pw = pw;
+        this.createdAt = new Timestamp(System.currentTimeMillis());
+        this.updateAt = this.createdAt;
     }
 
     public Employees() {
-        this.company_id = 0;
-        this.user = "";
-        this.pw = "";
+    	 this.id = 0;
+         this.companyId = 0;
+         this.user = "";
+         this.pw = "";
+         this.createdAt = new Timestamp(System.currentTimeMillis());
+         this.updateAt = this.createdAt;
     }
-public int getCompany_id() {
-	return company_id;
-}
-public void setCompany_id(int company_id) {
-	this.company_id = company_id;
-}
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(int companyId) {
+        this.companyId = companyId;
+    }
+
     public String getUser() {
         return user;
     }
@@ -43,4 +66,20 @@ public void setCompany_id(int company_id) {
     public void setPw(String pw) {
         this.pw = pw;
     }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+
+	public Timestamp getUpdateAt() {
+		return updateAt;
+	}
+	public void setUpdateAt(Timestamp updateAt) {
+		this.updateAt = updateAt;
+	}
 }
